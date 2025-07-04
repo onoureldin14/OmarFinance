06c4d0cd39491cd4d93f067b5fccccaa70c5d3ca
Omar Din <omardin@Omars-MacBook-Air.local>
2025-07-04 07:56:56 +0100

    fixing lambda error

--- tf-loan-app/backend.tf
+++ tf-loan-app/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-omarfinance-caf1725c"
+    bucket       = "tfstate-omarfinance-3e75e52e"
     key          = "infra/terraform.tfstate"
     region       = "eu-west-1"
     use_lockfile = true
--- tf-loan-app/terraform.tf
+++ tf-loan-app/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.0.0-beta3"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.7.1"
+    }    
   }
   required_version = ">= 0.13"
 }
